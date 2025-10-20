16e99a73289dc9862dce23cf2ebea5edf1ccaeee
Vladislav Pochernin <vspochernin@gmail.com>
2025-10-20 21:19:32 +0300

    upd

--- init.sh
+++ init.sh
@@ -9,6 +9,7 @@ curl -fsS https://start.spring.io/starter.tgz \
   -d bootVersion=3.5.6 \
   -d groupId=ru.vspochernin \
   -d artifactId=eureka-server \
+  -d description="Service registry of the Hotel Booking System" \
   -d name=eureka-server \
   -d javaVersion=21 \
   -d packaging=jar \
@@ -24,12 +25,13 @@ curl -fsS https://start.spring.io/starter.tgz \
   -d groupId=ru.vspochernin \
   -d artifactId=hotel-service \
   -d name=hotel-service \
+  -d description="Hotel and Room management service of the Hotel Booking System" \
   -d javaVersion=21 \
   -d packaging=jar \
   -d dependencies=web,data-jpa,h2,validation,security,cloud-eureka,actuator,lombok \
   | tar -xz -C hotel-service
 
-# 3) Booking Service (OpenFeign = cloud-feign)
+# 3) Booking Service
 mkdir -p booking-service
 curl -fsS https://start.spring.io/starter.tgz \
   -d type=maven-project \
@@ -38,6 +40,7 @@ curl -fsS https://start.spring.io/starter.tgz \
   -d groupId=ru.vspochernin \
   -d artifactId=booking-service \
   -d name=booking-service \
+  -d description="Booking and user management service of the Hotel Booking System" \
   -d javaVersion=21 \
   -d packaging=jar \
   -d dependencies=web,data-jpa,h2,validation,security,oauth2-resource-server,cloud-eureka,cloud-feign,actuator,lombok \
@@ -52,6 +55,7 @@ curl -fsS https://start.spring.io/starter.tgz \
   -d groupId=ru.vspochernin \
   -d artifactId=api-gateway \
   -d name=api-gateway \
+  -d description="API Gateway for routing and JWT propagation in the Hotel Booking System" \
   -d javaVersion=21 \
   -d packaging=jar \
   -d dependencies=cloud-gateway,security,cloud-eureka,actuator,lombok \
