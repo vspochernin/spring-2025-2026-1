75813b4ba481e5556a0ce671a8b7ea9d6caedba4
Vladislav Pochernin <vspochernin@gmail.com>
2025-10-20 21:36:57 +0300

    initialize services

---
+++ eureka-server/src/main/java/ru/vspochernin/eureka_server/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package ru.vspochernin.eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
---
+++ hotel-service/src/test/java/ru/vspochernin/hotel_service/HotelServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vspochernin.hotel_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
